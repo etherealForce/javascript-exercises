d86a762ac457e5d1b288d9119b6bbed3233ba023
Lu Win Maw <llaurence689@gmail.com>
2025-02-18 01:28:03 +0300

    yay!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(ary, ...args) {
+
+    //initiate the filter method for ary that checks each item whether its equal to del
+    ary = ary.filter((item) => !(args.includes(item)));
+
+    return ary;
 };
 
 // Do not edit below this line
