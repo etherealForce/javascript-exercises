9a23a08a449e264bd02ad31eb103d8de77351014
Lu Win Maw <llaurence689@gmail.com>
2025-02-17 22:33:18 +0300

    meow

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(strung, num) {
+    let productStrung = "";
 
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++) {
+        productStrung += strung;
+    }
+
+    return productStrung;
 };
 
 // Do not edit below this line
