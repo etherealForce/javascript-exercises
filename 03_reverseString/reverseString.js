9f38d2d8db68ee9c2d9f60227933553ade52f417
Lu Win Maw <llaurence689@gmail.com>
2025-02-17 22:58:23 +0300

    kek

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(aStrung) {
+    let letterz = [];
 
+    for (let aLetter of aStrung) {
+        letterz.push(aLetter);
+    }
+    letterz.reverse();
+
+    return letterz.join("");
+    //go through the array from the last until the first and 
 };
 
 // Do not edit below this line
