6ceb6cf64fd8fc8e3876b2e98aecbec10e11c32f
Lu Win Maw <llaurence689@gmail.com>
2025-02-18 23:08:37 +0300

    Eat leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(yar) {
+    if (yar % 100 === 0 && yar % 400 === 0) {
+        return true;
+    } else if (yar % 4 === 0 && yar % 100 !== 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
