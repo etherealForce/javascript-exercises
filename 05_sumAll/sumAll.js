4d6134c98e65a9ab269834f5cb75fabf3ce840bf
Lu Win Maw <llaurence689@gmail.com>
2025-02-18 19:56:38 +0300

    victory!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let sum = 0;
+    if (first > second) {
+        let temp = first;
+        first = second;
+        second = temp;
+    }
+
+    if ((first < 0 || second < 0) 
+        || (first % 1 !== 0 || second % 1 !== 0)
+        || (typeof first !== "number" || typeof second !== "number")) {
+        return "ERROR";
+    }
+
+    for(let i = first; i <= second; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
