000f224fa77f91d9c21f3d77246beafd1d53d120
Lu Win Maw <llaurence689@gmail.com>
2025-03-06 22:16:50 +0300

    Implement sum

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,10 @@ const subtract = function(a, b) {
 	return +a - +b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((all, current) => {
+    return all + current;
+  }, 0);
 };
 
 const multiply = function() {
