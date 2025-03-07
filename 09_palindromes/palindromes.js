174f71a5d54177f6cb0f6a4188f66bbbdaa9b50b
Lu Win Maw <llaurence689@gmail.com>
2025-03-07 21:58:46 +0300

    asd

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,9 @@
 const palindromes = function (strung) {
     
     let regex = /^[a-z0-9]+$/i;
-    let punctRemoved = strung.toLowerCase().split("")
+    let punctRemoved = strung
+    .toLowerCase()
+    .split("")
     .filter( (item) => regex.test(item))
     .join("");
     
