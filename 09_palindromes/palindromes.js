aff0fa179c22cf4f7f02e8108d735a42870765db
Lu Win Maw <llaurence689@gmail.com>
2025-03-07 21:53:57 +0300

    Conquered

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (strung) {
+    
+    let regex = /^[a-z0-9]+$/i;
+    let punctRemoved = strung.toLowerCase().split("")
+    .filter( (item) => regex.test(item))
+    .join("");
+    
 
+    let reversed = punctRemoved.split("").reverse().join("");
+
+    return punctRemoved === reversed;
 };
 
 // Do not edit below this line
