33e0ca2a6413d69e5b57fe150c23fa9b588738ff
Lu Win Maw <llaurence689@gmail.com>
2025-03-07 22:15:12 +0300

    FIBONAC

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(fibo) {
+    if (fibo < 0) {
+        return "OOPS";
+    }
+    let i = 0;
+    let sequence = [];
+    do {
+        
+        if (sequence.length < 2) {
+            sequence.push(i);
+        } else {
+            let current = sequence[i - 1] + sequence[i - 2];
+            sequence.push(current);
+        }
+        i++;
+    }   while (i <= fibo)
 
+    return sequence[fibo];
 };
 
 // Do not edit below this line
